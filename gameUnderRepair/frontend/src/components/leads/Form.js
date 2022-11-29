c5ca539b0f972c97a65e08800069835aefba6894
Wil <eddyw@wit.edu>
2022-11-28 21:17:57 -0500

    added frontend django app

---
+++ gameUnderRepair/frontend/src/components/leads/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+  render() {
+    return (
+      <h1>Add Lead Form</h1>
+    )
+  }
+}
+
+export default Form
---
+++ gameUnderRepair/frontend/src/components/leads/Leads.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Leads extends Component {
+  render() {
+    return (
+      <h1>Leads List</h1>
+    )
+  }
+}
+
+export default Leads
