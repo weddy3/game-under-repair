96b18720f5bf96d525cb9a0c62d57060e468573e
Wil <eddyw@wit.edu>
2022-11-30 22:55:18 -0500

    added leads from backend and added into state

--- gameUnderRepair/frontend/src/components/leads/Leads.js
+++ gameUnderRepair/frontend/src/components/leads/Leads.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getLeads } from '../../actions/leads';
 
 export class Leads extends Component {
+static ptropTypes = {
+  leads: PropTypes.array.isRequired
+};
+
+componentDidMount() {
+  this.props.getLeads();
+}
+
   render() {
     return (
       <h1>Leads List</h1>
-    )
+    );
   }
 }
 
-export default Leads
+const mapStateToProps = state => ({
+  leads: state.leads.leads
+});
+
+export default connect(mapStateToProps, { getLeads })(Leads);
