b9700a12a3423c0c41f52f857fac5982eb56c02d
Wil <eddyw@wit.edu>
2023-02-13 21:47:47 -0500

    crude way to calculate sg total

--- sgscripts/SGTotal.py
+++ sgscripts/SGTotal.py
@@ -24,22 +24,30 @@ lookup_90s = dict(zip(rounded_putting_df.Distance, rounded_putting_df.AvgPutts90
 # 'T' = Tee shot
 # 'F' = Fairway, 'R' = Rough, 'S' = Sand, 'W' = Recovery
 # 'P' = Putting
-sample_hole = {'1': [('T', 452), ('F', 164), ('NA'), ('P', 24)]}
+all_shots = {'1': [('T', 452), ('F', 164), ('NA'), ('P', 24)]}
 
 sgOTT = 0
 
 # TODO obviously need a cleaner way to group all shots relative to a SG group (solved above?)
 # Might not need to store 'T' and 'P' since order, depends on DB creation I guess
 # ie all shots that would be in the approach category togther while maintaining data, ('F', 250, 'F', 110)
-# to make the belove cleaner
+# to make the below cleaner
 # Could do something similar to below and assume this "lineraity", ie all tee shots, then all approach, then all short, then putting
 # would obviously have to populate nulls for when a ctageory has no shots for that hole, but I think would remove a loop and if check
 
-for hole in sample_hole:
-    for group in sample_hole[hole]:
+for hole in all_shots:
+    for group in all_shots[hole]:
         if group[0] == 'T':
             # SG Off The Tee for a hole is equal to expected number of strokes for that given yardage minus
             # the expected number of strokes to hole out for the following shot minus 1 to account for the actual shot
-            sgOTT += lookup_tee[group[1]] - lookup_fairway[sample_hole[hole][1][1]] - 1
+            sgOTT += lookup_tee[group[1]] - lookup_fairway[all_shots[hole][1][1]] - 1
 
-print(round(sgOTT,2))
+
+# conditonal for lie on approach and conditonals for checking for next value
+sg_ott = round(sum([lookup_tee[all_shots[hole][0][1]] - lookup_fairway[all_shots[hole][1][1]] - 1 for hole in all_shots]), 2)
+sg_approach = round(sum([lookup_fairway[all_shots[hole][1][1]] - lookup_pro2[all_shots[hole][3][1]] - 1 for hole in all_shots]), 2)
+sg_atg = 0.0
+sg_putting = round(sum([lookup_pro2[all_shots[hole][3][1]] - len(all_shots[hole][3]) + 1 for hole in all_shots]), 2)
+
+sg_total = round(sg_ott + sg_approach + sg_atg + sg_putting, 2)
+print(sg_total)
