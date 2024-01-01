206f5ef36c9cbe1747ec2bf996005b831fd8c7f5
Wil <eddyw@wit.edu>
2024-01-01 17:29:40 -0500

    created users app

--- golfsite/golfsite/settings.py
+++ golfsite/golfsite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "golf_round_post.apps.GolfRoundPostConfig",
+    "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
