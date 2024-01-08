19297f6eb3680c063fc075837250273ab1ff8c81
Wil <eddyw@wit.edu>
2024-01-07 22:46:52 -0500

    added in login and logout routes

--- golfsite/users/views.py
+++ golfsite/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth import logout
 
 
 def register(request):
@@ -10,9 +11,14 @@ def register(request):
             # this actually saves the inputted info and creates user -> also hashes password
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('golf-round-home')
+            messages.success(request, f'Account created for {username}! You are now able to login.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
