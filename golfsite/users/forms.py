e3e750ddb98f948b0b4fecf3b636cec7cc7de094
Wil <wileddy5699@gmail.com>
2024-01-16 18:18:59 -0500

    Added profile pictures for users

--- golfsite/users/forms.py
+++ golfsite/users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,3 +11,18 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    # TODO Broken, needs to be fixed, doesnt update photo
+    class Meta:
+        model = Profile
+        fields = ['image']
--- golfsite/users/models.py
+++ golfsite/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+
+    def save(self):
+        # runs parent classes save method
+        super().save()
+        # want to scale down potential large uploaded images
+        uploaded_profile_picture = Image.open(self.image.path)
+
+        if uploaded_profile_picture.height > 300 or uploaded_profile_picture.width > 300:
+            output_size = (300,300)
+            uploaded_profile_picture.thumbnail(output_size)
+            uploaded_profile_picture.save(self.image.path)
