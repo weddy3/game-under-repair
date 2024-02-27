17a98ae9fbc7c83c422ba10b156e0d30b573dcd1
Wil <wileddy5699@gmail.com>
2024-02-26 23:52:58 -0500

    initial attempt at db

--- golfsite/golf_round_post/models.py
+++ golfsite/golf_round_post/models.py
@@ -3,14 +3,51 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
 
-# eventually want all categories of sg, slope, rating, distance, etc etc
-# input fields in order to calulcate everything we need as well
+# TODO fill in all necessary params and double check logic
+
+class GolfShot(models.Model):
+    stroke_number = models.SmallIntegerField()
+    lie = models.TextField()
+    distance_remaining = models.SmallIntegerField()
+    penalty = models.BooleanField()
+    expected_strokes_to_hole_out = models.FloatField()
+
+
+class GolfHole(models.Model):
+    shot = models.ForeignKey(GolfShot, on_delete=models.CASCADE)
+    score = models.SmallIntegerField()
+    par = models.SmallIntegerField()
+    distance = models.SmallIntegerField()
+    total_strokes_gained = models.FloatField(default=0.0)
+    ott_strokes_gained = models.FloatField(default=0.0)
+    app_strokes_gained = models.FloatField(default=0.0)
+    atg_strokes_gained = models.FloatField(default=0.0)
+    put_strokes_gained = models.FloatField(default=0.0)
+    hole_number_in_golfers_round = models.SmallIntegerField()
+
+
+class GolfCourse(models.Model):
+    name = models.TextField()
+    distance = models.SmallIntegerField()
+    tee = models.TextField()
+    slope = models.SmallIntegerField()
+    rating = models.FloatField()
+    city = models.TextField()
+    state = models.TextField()
+
+
 class GolfRound(models.Model):
-    course = models.CharField(max_length=100, default='No Course Info')
+    course = models.OneToOneField(GolfCourse, on_delete=models.CASCADE, primary_key=True)
     score = models.SmallIntegerField(default=100)
     date_posted = models.DateField(default=timezone.now)
     total_strokes_gained = models.FloatField(default=0.0)
+    ott_strokes_gained = models.FloatField(default=0.0)
+    app_strokes_gained = models.FloatField(default=0.0)
+    atg_strokes_gained = models.FloatField(default=0.0)
+    put_strokes_gained = models.FloatField(default=0.0)
     golfer = models.ForeignKey(User, on_delete=models.CASCADE)
+    hole = models.ForeignKey(GolfHole, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return str(self.total_strokes_gained)
@@ -19,3 +56,4 @@ class GolfRound(models.Model):
     def get_absolute_url(self):
         # Redirects to detail view following creation of a golf round in the UI
         return reverse('golf-round-detail', kwargs={'pk': self.pk})
+    
